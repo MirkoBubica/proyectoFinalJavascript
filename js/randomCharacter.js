f693c2758dc37386db7c7861af6d58f5dd64e517
MirkoBubica <mirkobubicahundt@gmail.com>
2022-06-13 19:04:40 -0300

    Terminado

--- js/randomCharacter.js
+++ js/randomCharacter.js
@@ -9,7 +9,15 @@ console.log(`Random value between 0 and 27 is ${numeroRandomDIU}`);
 function getRandomNumber(){
  return numero = Math.floor(Math.random() * 27)
 }
+function showInfo(infos,number){
+  containerImagenes.innerHTML =`<img src="${infos[number].user_image}">
+  <img src="${infos[number].stand_image}">`;
+  containerCharaInfo.innerHTML =`<div class="user">Usuario: ${infos[number].user}</div>
+  <div class="stand">Stand: ${infos[number].stand}</div>
+  <div class="stand-type">Rango del Stand: ${infos[number].stand_type[0]}</div>
+  <div class="user-gender">Genero del Usuario: ${infos[number].gender}</div>`
 
+}
 async function getCharacter(){
   const number = getRandomNumber();
   var infos = await fetch('https://jojoapi.herokuapp.com/DiamondIsUnbreakable').then(response => response.json());
@@ -17,16 +25,6 @@ async function getCharacter(){
   console.log("Number = " + number);
   console.log(infos[number].user_image);
 
-  function showInfo(){
-    containerImagenes.innerHTML =`<img src="${infos[number].user_image}">
-    <img src="${infos[number].stand_image}">`;
-    containerCharaInfo.innerHTML =`<div class="user">Usuario: ${infos[number].user}</div>
-    <div class="stand">Stand: ${infos[number].stand}</div>
-    <div class="stand-type">Rango del Stand: ${infos[number].stand_type[0]}</div>
-    <div class="user-gender">Genero del Usuario: ${infos[number].gender}</div>`
 
-  }
-
-
-  showInfo(infos);
+  showInfo(infos,number);
 }
